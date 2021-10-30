9e5ba6cb5fa944d159be1f5a22daea1036e1b6b4
Zkrallah <muhammad.heshamyt@gmail.com>
2021-10-30 17:32:14 +0200

    Add Student to each Class Successfully

--- app/src/main/java/com/dimits/dimitsattendence/ClassActivity.java
+++ app/src/main/java/com/dimits/dimitsattendence/ClassActivity.java
@@ -1,15 +1,28 @@
 package com.dimits.dimitsattendence;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dimits.dimitsattendence.common.Common;
 import com.dimits.dimitsattendence.model.ClassModel;
+import com.dimits.dimitsattendence.model.StudentModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ClassActivity extends AppCompatActivity {
 
@@ -24,8 +37,18 @@ public class ClassActivity extends AppCompatActivity {
         setContentView(R.layout.layout_class_activity);
         class_name = (TextView) findViewById(R.id.class_name);
         students_btn = (Button) findViewById(R.id.students_btn);
+        add_students_btn = (Button)findViewById(R.id.add_students_btn);
         classModel = Common.currentClass;
+
         class_name.setText(classModel.getName());
+
+        add_students_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddStudentDialog();
+            }
+        });
+
         students_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +56,37 @@ public class ClassActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void showAddStudentDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Student");
+        builder.setMessage("Please, fill in the information");
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_add_student, null);
+        EditText etName =  itemView.findViewById(R.id.edt_student_name);
+        EditText etId =  itemView.findViewById(R.id.edt_student_id);
+        builder.setNegativeButton("CANCEL", (dialogInterface, i) ->
+                dialogInterface.dismiss());
+        builder.setPositiveButton("ADD", (dialogInterface, i) -> {
+            StudentModel studentModel = new StudentModel();
+            if (TextUtils.isEmpty(etName.getText().toString())){
+                Toast.makeText(ClassActivity.this, "Please Enter Student Name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            studentModel.setName(etName.getText().toString());
+            studentModel.setId(etId.getText().toString());
+            FirebaseDatabase.getInstance().getReference("Classes").child(classModel.getName())
+                    .child("students").child(etId.getText().toString())
+                    .setValue(studentModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    dialogInterface.dismiss();
+                    Toast.makeText(ClassActivity.this, "Student Created Successfully!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+        builder.setView(itemView);
+        androidx.appcompat.app.AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
